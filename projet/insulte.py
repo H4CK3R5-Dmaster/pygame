31a8693f96b7f8ce2048fe9feb4496a4b3ef1b92
H4CK3R5-FS <h4ck3r5pc@gmail.com>
2021-03-29 22:52:43 +0200

    game

--- projet/insulte.py
+++ projet/insulte.py
@@ -1,7 +1,11 @@
 class insulte :
-    def __init__(self,name,damage):
+    def __init__(self,name,damage,sujet,verbe,complement,point):
         self.name = name
         self.damage = damage
+        self.sujet = sujet
+        self.verbe = verbe
+        self.complement = complement
+        self.point = point
 
     def __str__(self):
         return self.name 
