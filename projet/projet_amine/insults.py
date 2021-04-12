6928d2de6f7619cc32c59be3a309d3150404b4c5
mohamedamine-tarhouni <mohamedamine.tarhouni@ynov.com>
2021-04-12 19:09:26 +0200

    projet_insultes

--- projet/projet_amine/insults.py
+++ projet/projet_amine/insults.py
@@ -3,22 +3,50 @@ from liste_insultes import Liste_insultes
 
 
 class INSULT:
-    def __init__(self):
-        self.Sujet = [Liste_insultes("tu", 6), Liste_insultes("ton père", 14), Liste_insultes("ta mère", 20), Liste_insultes(
-            "ton mari", 8), Liste_insultes("ta femme", 8), Liste_insultes("ta soeur", 10), Liste_insultes("ton frère", 10), Liste_insultes("ton chien", 6), Liste_insultes("ton chat", 6), Liste_insultes("ton hamster", 6)]
-        self.Verbe = [Liste_insultes("es(t)", 3), Liste_insultes("a(s)", 3), Liste_insultes("n'es(t) pas", 3), Liste_insultes("n'a(s) pas", 3), Liste_insultes(
-            "n'es(t) pas", 3), Liste_insultes("a(s) triché", 3), Liste_insultes("n'a(s) pas regardé", 4), Liste_insultes("rassemble(s) à", 4), Liste_insultes("ne connais pas", 4), Liste_insultes("rassemble(s) à", 0), Liste_insultes("rassemble(s) à", 0)]
-        self.Verbe_adj = [Liste_insultes("es(t) lent comme", 5), Liste_insultes(
-            "es(t) stupide comme", 5), Liste_insultes("es(t) con comme", 4), Liste_insultes("est bete comme", 6), Liste_insultes("est aveugle comme", 5), Liste_insultes("est aveugle comme", 0), Liste_insultes("est aveugle comme", 0)]
-        self.Adj = [Liste_insultes("stupide", 10), Liste_insultes("un(e) con", 8), Liste_insultes(
-            "intelligent(e)", 4), Liste_insultes("petit comme", 5), Liste_insultes("grand", 7), Liste_insultes("un(e) idiot", 7), Liste_insultes("un(e) idiot", 0), Liste_insultes("un(e) idiot", 0)]
-        self.COD = [Liste_insultes("star wars", 6), Liste_insultes("le train", 5), Liste_insultes("l'intelligence", 5), Liste_insultes("l'honneur", 7), Liste_insultes("Prison Break", 6), Liste_insultes("La casa del papel", 6), Liste_insultes(
-            "la maison", 4), Liste_insultes("la merde", 5), Liste_insultes("une pyramide", 7), Liste_insultes("en cp pour réussir", 7), Liste_insultes("de vie", 5), Liste_insultes("la vache", 6)]
+    def __init__(self,adv):
+#les faiblesses des caractères(faut mettre la faiblesse de chacun dans son tableau) et vaut mieux de copier coller des tableaux des insultes
+#car ca peut causer des problèmes si les mots de faiblesse ne sont pas identiques à celle des tableaux d'insultes
+        #faiblesses du dame
+        if adv== "Dame":
+            self.faiblesse=["ton chien"]
 
+
+        #faiblesse du gentleman
+        if adv=="gentleman":
+           self.faiblesse=["ta femme","grand","l'honneur","ton père","star wars"]
+
+        #faiblesse du poor
+        if adv=="poor":
+           self.faiblesse=["ta femme","grand","l'honneur","ton père","star wars"]
+
+        #faiblesse du gangster
+        if adv=="gangster":
+           self.faiblesse=["ta femme","grand","l'honneur","ton père","star wars"]
+
+        #faiblesse du master ocean
+        if adv=="master ocean":
+           self.faiblesse=["ta femme","grand","l'honneur","ton père","star wars"]
+
+#pour ajouter un mot à la liste faut juste appeler Listes_insultes et mettre l'insulte et sa dégat
+#vous pouvez copier ca et le mettre ou vous voulez ajouter l'insulte : , Liste_insultes("insulte", 0)
+        self.Sujet = [Liste_insultes("tu", 6), Liste_insultes("ton père", 14), Liste_insultes("ta mère", 20), Liste_insultes(
+            "ton mari", 8), Liste_insultes("ta femme", 8), Liste_insultes("ta soeur", 10), Liste_insultes("ton frère", 10), Liste_insultes("ton chien", 6), Liste_insultes("ton chat", 6), Liste_insultes("ton hamster", 6), Liste_insultes("ton voisin", 6)]
+            #si vous faites des changements dans les verbes veuillez me prevenir(juste les verbes pas verbe_adj)
+        self.Verbe = [Liste_insultes("es(t)", 3), Liste_insultes("n'es(t) pas", 3), Liste_insultes(
+            "n'es(t) pas", 3), Liste_insultes("a(s)", 3), Liste_insultes("n'a(s) pas", 3), Liste_insultes("a(s) triché", 3), Liste_insultes("n'a(s) pas regardé", 4), Liste_insultes("rassemble(s) à", 4), Liste_insultes("ne connais pas", 4), Liste_insultes(" ", 0), Liste_insultes(" ", 0)]
+        self.Verbe_adj = [Liste_insultes("es(t) lent comme", 5), Liste_insultes(
+            "es(t) stupide comme", 5), Liste_insultes("es(t) con comme", 4), Liste_insultes("est bete comme", 6), Liste_insultes("est aveugle comme", 5), Liste_insultes(" ", 0), Liste_insultes(" ", 0)]
+        self.Adj = [Liste_insultes("une stupide", 10), Liste_insultes("un(e) con", 8), Liste_insultes(
+            "intelligent(e)", 4), Liste_insultes("petit", 5), Liste_insultes("grand", 7), Liste_insultes("un(e) idiot", 7), Liste_insultes(" ", 0), Liste_insultes(" ", 0)]
+        self.COD = [Liste_insultes("star wars", 6), Liste_insultes("le train", 5), Liste_insultes("l'intelligence", 5), Liste_insultes("l'honneur", 7), Liste_insultes("Prison Break", 6), Liste_insultes("La casa del papel", 6), Liste_insultes(
+            "la maison", 4), Liste_insultes("la merde", 5), Liste_insultes("une pyramide", 7), Liste_insultes("toi", 9), Liste_insultes("la/de vie", 5), Liste_insultes("la vache", 6)]
+            
+            # ne pas toucher ca
+        self.liaison=["et","et","et","et","et","et","et","et","et","et","et","et","et","et"]
     def generate_random_list(self):
         Random_List = []
-        if len(self.Sujet)>1:
-            sujet = randint(4, 6)
+        if len(self.Sujet)>0:
+            sujet = randint(2, 4)
             suj_val = randint(0, len(self.Sujet)-1)
             for j in range(0, sujet, 1):
                 Random_List.append(self.Sujet[suj_val].name)
@@ -26,8 +54,8 @@ class INSULT:
                 suj_val = randint(0, len(self.Sujet)-1)
             # while self.Sujet[suj_val].name in Random_List:
             #     suj_val=randint(0,len(self.Sujet)-1)
-        if len(self.Verbe)>2:
-            verbe = randint(3, 4)
+        if len(self.Verbe)>0:
+            verbe = randint(1, 2)
             ver_val = randint(0, len(self.Verbe)-1)
             for vb in range(0, verbe, 1):
                 Random_List.append(self.Verbe[ver_val].name)
@@ -35,8 +63,8 @@ class INSULT:
                 ver_val = randint(0, len(self.Verbe)-1)
             # while self.Verbe[ver_val].name in Random_List:
             #     ver_val=randint(1,len(self.Verbe)-1)
-        if len(self.Verbe_adj)>2:
-            verbe_adj = randint(2, 3)
+        if len(self.Verbe_adj)>0:
+            verbe_adj = randint(1, 2)
             ver_adj_val = randint(0, len(self.Verbe_adj)-1)
             for vb_adj in range(0, verbe_adj, 1):
                 Random_List.append(self.Verbe_adj[ver_adj_val].name)
@@ -44,8 +72,8 @@ class INSULT:
                 ver_adj_val = randint(0, len(self.Verbe_adj)-1)
             # while self.Verbe[ver_adj_val].name in Random_List:
             #     ver_adj_val=randint(1,len(self.Verbe_adj)-1)
-        if len(self.Adj)>2:
-            adjective = randint(2, 3)
+        if len(self.Adj)>0:
+            adjective = randint(1, 3)
             adj_val = randint(0, len(self.Adj)-1)
             for adj in range(0, adjective, 1):
                 Random_List.append(self.Adj[adj_val].name)
@@ -53,8 +81,8 @@ class INSULT:
                 adj_val = randint(0, len(self.Adj)-1)
                 # while self.Adj[adj_val].name in Random_List:
                 #     adj_val=randint(1,len(self.Adj)-1)
-        if len(self.COD)>2:
-            cod_rep = randint(2, 3)
+        if len(self.COD)>0:
+            cod_rep = randint(1, 3)
             cod_val = randint(0, len(self.COD)-1)
             for cod_i in range(0, cod_rep, 1):
                 Random_List.append(self.COD[cod_val].name)
@@ -62,6 +90,13 @@ class INSULT:
                 cod_val = randint(0, len(self.COD)-1)
                 # while self.COD[cod_val].name in Random_List:
                 #     cod_val=randint(1,len(self.COD)-1)
+        if len(self.liaison)>0:
+            lie_rep=randint(0,2)
+            lie_val=randint(0,len(self.liaison)-1)
+            for lie_i in range(0,lie_rep,1):
+                Random_List.append(self.liaison[lie_val])
+                self.liaison.remove(self.liaison[lie_val])
+                lie_val = randint(0, len(self.liaison)-1)
         return Random_List
 
     def generate_insults_list(self):
--- projet/projet_amine/main.py
+++ projet/projet_amine/main.py
@@ -1,5 +1,6 @@
 from Player import PLAYER
 from insults import INSULT
+from random import randint
 characters = ["Dame", "gentleman", "poor", "gangster", "king", "master ocean"]
 
 # cette fonction affiche les insultes disponibles
@@ -64,9 +65,9 @@ print(name_player1, "-->", character1)
 print(name_player2, "-->", character2)
 PLAYER1 = PLAYER(name_player1, character1)
 PLAYER2 = PLAYER(name_player2, character2)
-insult = INSULT()
-insult1 = INSULT()
-insult2 = INSULT()
+insult = INSULT("")
+insult1 = INSULT(character2)
+insult2 = INSULT(character1)
 
 
 def display_array_in_string(arr):
@@ -102,55 +103,223 @@ def sum_insults(arr, insult):
             if insult_index.name == arr[i]:
                 sum += insult_index.damage
     return sum
-# cette fonction permet de gérer le combat
 
 
-def Combat(PLAYER1, insult1, PLAYER2, insult2):
+# cette fonction verifie sile mot ajoutée est une faiblesse du caractère adversaire
+def verif_weakness(str, ins):
+    for weak in ins.faiblesse:
+        if weak == str:
+            return True
+    return False
+
+
+# cette fonction forme un tableau avec que les insultes sans les points des degats
+def form_array_insults(key):
     list = []
-    tabP1 = []
-    tabP2 = []
-    while PLAYER1.pv > 0 and PLAYER2.pv > 0:
+    for subkey in key:
+        list.append(subkey.name)
+    return list
+
+# cette fonction fait la verification du toute la phrase formée par les joueurs
+
+
+def verif_longue_phrases(arr, ins):
+
+    # une liste auxiliaire pour ne pas changer les tableaux des joueurs
+    aux_list = arr
+
+    # la valeur de v sera faut s'il ya une faute dans la phrase du joueur
+    v = True
+
+    #s'il ya des "et" alors la phrase doit etre verifié plusieurs fois alors on appelle une boucle
+    if "et" in aux_list and aux_list.index("et") % 3 == 0 and aux_list.index("et") != 0:
+        while "et" in aux_list and aux_list.index("et") < len(aux_list)-1 and v == True:
+
+            # on verifie la phrase partie par partie
+            v = verif_forme(aux_list[slice(0, aux_list.index("et"))], ins)
+            if v == True:
+                aux_list = aux_list[slice(
+                    aux_list.index("et")+1, len(aux_list))]
+                v = verif_forme(aux_list, ins)
+    else:
+
+        #si c'est une phrase simple on fait qu'une seule verification
+        v = verif_forme(aux_list, ins)
+    return v
+
+
+# cette fonction verifie la forme d'une partie de la phrase (les phrases doivent etre sous forme de sujet+verbe+adj+cod ou sujet+verbe/adj + sujet ou cod)
+def verif_forme(arr, ins):
+
+
+    # c'est les des insultes mais sans la classe listes_insultes ou les dégats juste les insultes
+    # pour verifier le bon emplacement des mots choisis
+    sujet_array = form_array_insults(ins.Sujet)
+
+    verbe_array = form_array_insults(ins.Verbe)
+
+    verbe_adj_array = form_array_insults(ins.Verbe_adj)
+
+    adj_array = form_array_insults(ins.Adj)
+
+    cod_array = form_array_insults(ins.COD)
+
+    v1 = True  # verifie l'emplacement du sujet
+    v2 = True  # verifie l'emplacement du verbe
+    v3 = True  # verifie l'emplacement du cod ou adj
+    v4 = True  # verifie l'emplacement du "et"
+# on parcour le tableau du joueur
+    for i in range(0, len(arr), 1):
+        # la premiere case doit etre un sujet
+        if i == 0:
+            if (arr[i] in sujet_array) == False:
+                v1 = False
+
+        # la deuxieme case doit etre un verbe
+        if i == 1:
+            if (arr[i] in verbe_array or arr[i] in verbe_adj_array) == False:
+                v2 = False
+
+        # la troisieme case doit etre un adj ou cod dependant du verbe selectionné
+        if i == 2:
+            if ((arr[i-1] in verbe_array[slice(2)] and arr[i] in adj_array) or ((arr[i-1] in verbe_adj_array or arr[i-1] in verbe_array[slice(3, len(verbe_array))]) and (arr[i] in adj_array or arr[i] in cod_array)) or (arr[i-1] in verbe_adj_array and arr[i] in sujet_array and arr[i] != "tu")) == False:
+                v3 = False
+
+        # la quatrieme case doit etre un "et"
+        if i == 3:
+            if arr[i] != "et":
+                v4 = False
+
+        # si une des conditions n'est pas bonne ca donnera false
+        if (v1 == False or v2 == False or v3 == False or v4 == False):
+            return False
+    return True
+
+
+# cette fonction permet de gérer le combat
+def Combat(PLAYER1, insult1, PLAYER2, insult2):
+    # le nombre de mots ajoutés par joueur1 (sans compter les "et")
+    words_P1 = 0
+
+    # le nombre de mots ajoutés par joueu21 (sans compter les "et")
+    words_P2 = 0
+
+    list = []  # la liste des mots
+
+    tabP1 = []  # la liste des mots du joueur 1
+
+    tabP2 = []  # la liste des mots du joueur 2
+
+    alt_list = ""  # liste qui va arreter le jeu dans le cas y'a plus du mots dans la liste
+
+    s1 = 0  # nombre de dégats fait par joueur 1
+
+    s2 = 0  # nombre de dégats fait par joueur 2
+
+    multi_P1 = 1  # en cas de coup critique ca va multiplier le dégat
+
+    multi_P2 = 1  # en cas de coup critique ca va multiplier le dégat
+
+    # on arrete dès que la liste est vide ou un des deux joueurs est morts
+    while PLAYER1.pv > 0 and PLAYER2.pv > 0 and alt_list != []:
+
+        # l'affichage des messages de chaque joueur
         display_messages(PLAYER1.name, display_array_in_string(
             tabP1), PLAYER2.name, display_array_in_string(tabP2))
-        print(PLAYER1.name, " = ", PLAYER1.pv)
-        print(PLAYER2.name, " = ", PLAYER2.pv)
+
+        # l'affichage des points de vie de chaque joueur
+        print(PLAYER1.name, " HP = ", PLAYER1.pv)
+        print(PLAYER2.name, " HP = ", PLAYER2.pv)
+
+        # lorsque la liste est vide on génére d'autre mots
         if list == []:
             list = insult.generate_insults_list()
+            alt_list = list
+
         if list != []:
+
+            # l'affichage des mots disponibles
             Display_insult_list(list)
+
+            # choix du joueur 1
             choix1 = saisie_char_num(
                 len(list), "Le choix de "+PLAYER1.name+" : ")
+
+            # on ajout le mot selectionné dans le tableau du joueur 1
             tabP1.append(list[choix1-1])
+
+            # si le mot choisi est bonne on l'ajoute au tableau sinon on affiche "veuillez apprendre le grammaire"
+            if verif_longue_phrases(tabP1, insult1) == True:
+                if list[choix1-1] != "et":
+                    words_P1 = words_P1+1
+                if verif_weakness(list[choix1-1], insult1):
+                    multi_P1 = randint(2, 4)
+                    print("WEAKNESS !! * ", multi_P1)
+            else:
+                print("veuillez apprendre la grammaire !! ")
+                tabP1.pop(len(tabP1)-1)
+
             display_messages(PLAYER1.name, display_array_in_string(
                 tabP1), PLAYER2.name, display_array_in_string(tabP2))
-            if len(tabP1) >= 3:
+
+            # le joueur a la possibilité d'attaquer s'il a une bonne phrase dans son tableau
+            if words_P1 % 3 == 0 and words_P1 > 0 and tabP1[len(tabP1)-1] != "et":
                 choixattack1 = saisie_char_num(
                     2, PLAYER1.name+" Tu veux attaquer?:\n\n1-->oui\n\n2-->non  ")
                 if choixattack1 == 1:
-                    s1 = sum_insults(tabP1, insult1)
-                    print(PLAYER1.name,"a fait ",s1," de dégats à ",PLAYER2.name)
+
+                    # on fait la somme des degats des insultes choisis
+                    s1 = (sum_insults(tabP1, insult1)*multi_P1)
+                    print(PLAYER1.name, "a fait ", s1,
+                          " de dégats à ", PLAYER2.name)
+
+                    # on diminue les points de vie du joueur adversaire
                     PLAYER2.pv -= s1
+
+                    multi_P1 = 1
                     tabP1 = []
-            print(PLAYER1.name, " = ", PLAYER1.pv)
-            print(PLAYER2.name, " = ", PLAYER2.pv)
+                    words_P1 = 0
+
+            print(PLAYER1.name, " HP = ", PLAYER1.pv)
+            print(PLAYER2.name, " HP = ", PLAYER2.pv)
+
+            # on enlève le mot selectionné de la liste
             list.remove(list[choix1-1])
 
+            # on fait les memes etapes pour le deuxième joueur
             if list == []:
                 list = insult.generate_insults_list()
+                alt_list = list
             Display_insult_list(list)
-
             choix2 = saisie_char_num(
                 len(list), "Le choix de "+PLAYER2.name+" : ")
             tabP2.append(list[choix2-1])
-            if len(tabP2) >= 3:
+            if verif_longue_phrases(tabP2, insult2) == True:
+                if list[choix2-1] != "et":
+                    words_P2 = words_P2+1
+                if verif_weakness(list[choix2-1], insult2):
+                    multi_P2 = randint(2, 4)
+                    print("WEAKNESS !! * ", multi_P2)
+            else:
+                print("veuillez apprendre la grammaire !! ")
+                tabP2.pop(len(tabP2)-1)
+            print(words_P1)
+            print(words_P2)
+            if words_P2 % 3 == 0 and words_P2 > 0 and tabP2[len(tabP2)-1] != "et":
                 choixattack2 = saisie_char_num(
                     2, PLAYER2.name+" Tu veux attaquer?:\n\n1-->oui\n\n2-->non  ")
                 if choixattack2 == 1:
-                    s2 = sum_insults(tabP2, insult2)
-                    print(PLAYER2.name,"a fait ",s2," de dégats à ",PLAYER1.name)
+                    s2 = (sum_insults(tabP2, insult2)*multi_P2)
+                    print(PLAYER2.name, "a fait ", s2,
+                          " de dégats à ", PLAYER1.name)
                     PLAYER1.pv -= s2
+                    multi_P2 = 1
                     tabP2 = []
+                    words_P2 = 0
             list.remove(list[choix2-1])
+            if list == []:
+                list = insult.generate_insults_list()
+                alt_list = list
 
 
 Combat(PLAYER1, insult1, PLAYER2, insult2)
